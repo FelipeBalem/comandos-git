d10747123c8573569989f1a260a24ffa7639dc2d
Felipe Balem <felipe.balem.si@gmail.com>
2021-11-24 15:33:49 -0400

    Adicionado o arquivo branchteste.py no branchteste

---
+++ branchteste.py
@@ -0,0 +1,3 @@
+print("Agora você está no branch teste, eu só existo aqui!")
+print("Lembre-se de retornar ao branch master para continuar fazendo anotações")
+print("O comando é git checkout master")
